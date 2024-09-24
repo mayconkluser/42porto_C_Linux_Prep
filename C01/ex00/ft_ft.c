494c3fdfdd19893693ff4a4ebc172f49d749dec5
mayconkluser <maycon10kluser@gmail.com>
2024-09-24 19:05:02 +0100

    add ex00 ft_ft.c

---
+++ C01/ex00/ft_ft.c
@@ -0,0 +1,19 @@
+void	ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+
+/*
+#include <stdio.h>
+int	main()
+{
+	int	nbr_;
+	int	*nbr_nbr;
+
+	nbr_ = 24;
+	nbr_nbr = &nbr_;
+
+	ft_ft(nbr_nbr);
+	printf("%d", nbr_);
+}
+*/
