bf74223e62833f6b5442e1a4ff00965e5ab5962e
mayconkluser <maycon10kluser@gmail.com>
2024-09-26 19:30:54 +0100

    add ex05 ft_sqrt.c

---
+++ C05/ex05/ft_sqrt.c
@@ -0,0 +1,27 @@
+int ft_sqrt(int nb)
+{
+    int i;
+
+    if (nb < 0)
+        return (0);
+
+    i = 1;
+    while (i * i <= nb)
+    {
+        if (i * i == nb)
+            return (i);
+        i++;
+    }
+    return (0);
+}
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+int	main(int argc, char **argv)
+{
+	if (argc > 1)
+		printf("%d", ft_sqrt(atoi(argv[1])));
+	return (0);
+}
+*/
