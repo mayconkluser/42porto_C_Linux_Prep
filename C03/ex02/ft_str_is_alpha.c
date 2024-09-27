d60e24a236f40a83484da45c8e59cd7997108544
mayconkluser <maycon10kluser@gmail.com>
2024-09-28 00:27:32 +0100

    add ex01 ft_str_is_alpha.c

---
+++ C03/ex02/ft_str_is_alpha.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+int ft_str_is_alpha(char *str)
+{
+	int i;
+
+	while(str[i] != '\0')
+	{
+		if (!(str[i] >= 65 && str[i] <= 90)
+			&& !(str[i] >= 97 && str[i] <= 122))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc > 1)
+		printf("%d", ft_str_is_alpha(argv[1]));
+	return (0);
+}
