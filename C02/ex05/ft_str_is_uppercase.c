ca728244da9b0c46439d7cdb84b8c15a69f30511
mayconkluser <maycon10kluser@gmail.com>
2024-09-28 23:15:01 +0100

    add ex05 ft_str_is_uppercase.c

---
+++ C02/ex05/ft_str_is_uppercase.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int ft_str_is_uppercase(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= 65 && str[i] <= 90))
+			return(0);
+		i++;
+	}
+	return (1);
+}
+
+// Remove this function, it's just for testing.
+int main(int argc, char **argv)
+{
+	if (argc > 1)
+		printf("%d", ft_str_is_uppercase(argv[1]));
+	return (0);
+}
