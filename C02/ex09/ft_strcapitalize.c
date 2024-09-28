4ff7492f67aa07604891fe55c32ddf2d35139fb4
mayconkluser <maycon10kluser@gmail.com>
2024-09-29 00:36:27 +0100

    add ex09 ft_strcapitalize.c

---
+++ C02/ex09/ft_strcapitalize.c
@@ -0,0 +1,36 @@
+#include <stdio.h> 
+
+char *ft_strcapitalize(char *str)
+{
+	int i;
+	int new_word;
+
+	i = 0;
+	new_word = 1;
+	while (str[i] != '\0')
+	{
+		if ((str[i] >= '0' && str[i] <= '9') 
+			|| (str[i] >= 'a' && str[i] <= 'z')
+			|| (str[i] >= 'A' && str[i] <= 'Z'))
+		{
+			if (new_word && (str[i] >= 'a' && str[i] <= 'z'))
+			{
+				str[i] -= 32;
+				new_word = 0;
+			}
+			else if (str[i] >= 'A' && str[i] <= 'Z')
+				str[i] += 32;
+		}
+		else
+			new_word = 1;
+		i++;
+	}
+	return (str);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc > 1)
+		printf("%s", ft_strcapitalize(argv[1]));
+	return (0);
+}
