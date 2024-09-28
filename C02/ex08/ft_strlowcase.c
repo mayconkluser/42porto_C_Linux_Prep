fd8f333565d314d9f5077809bbacd670ab81760a
mayconkluser <maycon10kluser@gmail.com>
2024-09-28 23:56:18 +0100

    add ex08 ft_strlowcase.c

---
+++ C02/ex08/ft_strlowcase.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+char *ft_strlowcase(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 65 && str[i] <= 90)
+			str[i] += 32;
+		i++;
+	}
+	return (str);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc > 1)
+		printf("%s", ft_strlowcase(argv[1]));
+	return (0);
+}
