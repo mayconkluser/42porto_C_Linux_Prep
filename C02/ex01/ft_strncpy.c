de7dbd241dd4e62d5a6e91cc1865958472832961
mayconkluser <maycon10kluser@gmail.com>
2024-09-29 19:06:30 +0100

    remove main and fixed function

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -1,22 +1,28 @@
-#include <unistd.h> 
+#include <stdio.h> 
 #include <stdlib.h> 
+
 char *ft_strncpy(char *dest, char *str, unsigned int n)
 {
 	int i = 0;
 	while (str[i] != '\0' && n != i)
 	{
 		dest[i] = str[i];
-		// Remove this function, it's just for testing.
-		write(1, &dest[i], 1);
 		i++;
 	}
-	dest[i] = '\0';
-	return (0);
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
 
+/*
 int main(int argc, char **argv)
 {
 	if (argc > 1)
-		ft_strncpy(argv[1], argv[2], atoi(argv[3]));
+		printf("%s", ft_strncpy(argv[1], argv[2], atoi(argv[3])));
 	return (0);
 }
+ */
